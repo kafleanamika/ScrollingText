e21e30e4337fd63326180fa7c0238593a4e7ed9d
Anamika Kafle <kafleanamika123@gmail.com>
2021-03-18 21:24:22 +0545

    fourth commit

---
+++ app/build/generated/source/buildConfig/debug/me/mahakagg/scrollingtext2HW/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.mahakagg.scrollingtext2HW;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "me.mahakagg.scrollingtext2HW";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
